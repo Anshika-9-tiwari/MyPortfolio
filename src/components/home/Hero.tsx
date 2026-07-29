60b118a870bb9c9da56065548c4c3910904d0db0
Anshika Tiwari <tiwarianshikatiwari6@gmail.com>
2026-07-30 00:13:28 +0530

    My_web 10th vercel_web_analytics

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
             </h1>
 
             {/* Description */}
-            <div className="mt-6 max-w-xl space-y-3 text-[15px] leading-7 text-gray-400 sm:text-base sm:leading-8 lg:text-[18px]">
+            <div className="max-w-xl space-y-3 mt-8 text-lg leading-7 text-gray-400">
 
               <p>
                 UMAKAN Technologies provides website development, website
